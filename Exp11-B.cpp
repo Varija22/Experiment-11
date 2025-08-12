52d915766ae7d16a5520fc738b636faf86b5f22e
Varija22 <varija.korti.btech2024@sitpune.edu.in>
2025-08-12 14:56:59 +0530

    Add files via upload

---
+++ Exp11-B.cpp
@@ -0,0 +1,22 @@
+//Varija K
+//24070123165
+
+#include <iostream>
+using namespace std;
+class cube{
+    public:
+    int height=2;
+    int width=3;
+    int length=5;
+
+int volume(){
+    int v;
+    v=height*width*length;
+    return v;
+}
+};
+int main() {
+  cube c1;
+  int vol=c1.height*c1.width*c1.length;
+  cout<<"Volume:"<<vol<<endl;
+}
