ff5f8e88972168cab1e6ab6a60657c7c70551b7e
Varija22 <varija.korti.btech2024@sitpune.edu.in>
2025-08-12 15:36:11 +0530

    Create Exp11-D.cpp

---
+++ Exp11-D.cpp
@@ -0,0 +1,36 @@
+//Varija K
+//24070123165
+#include <iostream>
+using namespace std;
+
+class Volume {
+public:
+    int length;
+    int breadth;
+    int height;
+
+    void input() {
+        cout << "Enter the value of length: ";
+        cin >> length;
+        cout << "Enter the value of breadth: ";
+        cin >> breadth;
+        cout << "Enter the value of height: ";
+        cin >> height;
+    }
+
+    int vol() {
+        int v = length * breadth * height;
+        return v;
+    }
+
+    void display() {
+        int a = vol();
+        cout << "The volume is " << a;
+    }
+};
+
+int main() {
+    Volume volume1;
+    volume1.input();
+    volume1.display();
+}
