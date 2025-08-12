ebc56271c835e179ef1de480e5b6a1fdf9ffa3fc
Varija22 <varija.korti.btech2024@sitpune.edu.in>
2025-08-12 15:34:28 +0530

    Update Exp11-C.cpp

--- Exp11-C.cpp
+++ Exp11-C.cpp
@@ -6,34 +6,22 @@ using namespace std;
 
 class cube {
 private:
-    int height;
-    int width;
-    int length;
+    int height = 2.0;
+    int width = 3.0;
+    int length = 5.0;
 
 public:
-    
-    void getData() {
-        cout << "Enter height: ";
-        cin >> height;
-        cout << "Enter width: ";
-        cin >> width;
-        cout << "Enter length: ";
-        cin >> length;
-    }
-
-    
     int volume() {
-        return height * width * length;
+        int v;
+        v = height * width * length;
+        return v;
     }
 };
 
 int main() {
     cube cube1;
-    cube1.getData();  
-
-    int vol = cube1.volume();  // Calculate volume
+    int vol = cube1.volume();
     cout << "Volume: " << vol << endl;
+} 
 
-    return 0;
-}
 
